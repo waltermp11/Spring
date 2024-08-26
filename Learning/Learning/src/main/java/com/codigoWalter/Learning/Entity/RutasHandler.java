563773c24402e24260c23e273f788252d334f4cf
waltermp11 <walterm200011@gmail.com>
2024-08-26 15:33:41 -0500

    Learning Spring boot framework

---
+++ Learning/Learning/src/main/java/com/codigoWalter/Learning/Entity/RutasHandler.java
@@ -0,0 +1,25 @@
+package com.codigoWalter.Learning.Entity;
+
+import com.codigoWalter.Learning.Rutas;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RutasHandler {
+    private final Logger logger = LoggerFactory.getLogger(RutasHandler.class);
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handlerNullPointerExcepcion(NullPointerException exception) {
+        logger.error(exception.getMessage());
+        return "Internar Error. Contact Support";
+
+
+    }
+
+    //Aca es donde nosotros vamos a tener el control de los errores o excepciones para que no lleguen a nuestro cliente final
+}
--- Learning/Learning/src/main/java/com/codigoWalter/Learning/Rutas.java
+++ Learning/Learning/src/main/java/com/codigoWalter/Learning/Rutas.java
@@ -16,38 +16,67 @@ public class Rutas {
     private final Logger logger = LoggerFactory.getLogger(Rutas.class);
 
     @GetMapping("/saludo")
-    public String saludando(){
+    public String saludando() {
         return "Hola Mundo";
 
     }
 
-    @GetMapping ("/libro/{id}/editorial/{editorial}")
-    public String leerLibro (@PathVariable int id, @PathVariable String editorial){
-        return  "Leyendo el libro con el id " + id + " con la editorial : " + editorial;
+    @GetMapping("/libro/{id}/editorial/{editorial}")
+    public String leerLibro(@PathVariable int id, @PathVariable String editorial) {
+        return "Leyendo el libro con el id " + id + " con la editorial : " + editorial;
 
 
     }
 
-    @GetMapping ("/params/{id}")
-    public String testingsRequestParams(@PathVariable int id, @RequestParam  String nombre, @RequestParam String apellido){
-            return "Parametros con el id " + id + "parametros " + nombre +  " " + apellido;
+    @GetMapping("/params/{id}")
+    public String testingsRequestParams(@PathVariable int id, @RequestParam String nombre, @RequestParam String apellido) {
+        return "Parametros con el id " + id + "parametros " + nombre + " " + apellido;
 
-        }
+    }
+
     @PostMapping("/libro")
-    public ResponseEntity<String>  guardarLibro(@RequestBody Libro libro){
+    public ResponseEntity<String> guardarLibro(@RequestBody Libro libro) {
 
-        if (libro.getCantidadDeLibros() ==1){
+        if (libro.getCantidadDeLibros() == 1) {
             logger.error("El libro esta vacion ");
-             return new ResponseEntity<>("El libro esta vacio", BAD_REQUEST);
-
+            return new ResponseEntity<>("El libro esta vacio", BAD_REQUEST);
 
 
         }
         logger.info("Se guardo el libro exitosamente");
         return new ResponseEntity<>("Se guardo el libro correctamente! " + libro.toString(), OK);
+    }
+
+    @GetMapping("/animales/{lugar}")
+    public ResponseEntity<String> mostrandoAnimalesSegunLugar(@PathVariable String lugar) {
+
+        if (lugar.equals("granja")) {
+
+            return ResponseEntity.status(OK).body("Caballo, vaca, gallina, pato");
+        } else if (lugar.equals("zoologico")) {
+            return ResponseEntity.status(OK).body("Tigre, leon, mono, elefante");
+
+
+        } else if (lugar.equals("casa")) {
+            return ResponseEntity.status(OK).body("Perro, Gato, Peces");
+
+        } else {
+            return ResponseEntity.status(BAD_REQUEST).body("Lugar no conocido 🫥");
+        }
+    }
+
+    @GetMapping("/calcular/{numero}")
+    public Integer calculandoNumero( @PathVariable Integer numero){
+        if (numero<0){
+
+            throw new  NullPointerException("el numero es negativo 🫥");
         }
 
+        logger.info("El numero es entero positivo");
+        return numero;
+
     }
+}
 
 
 
